c2fea7143016a0dd38a9b7cda6dd5eed71b1af72
tukamilano <milanotukamoto@g.ecc.u-tokyo.ac.jp>
2025-10-06 21:04:54 +0900

    pyproverを通常のディレクトリとして追加、README.mdも追加

---
+++ pyprover/interactive.py
@@ -0,0 +1,88 @@
+import readline
+from prover import Prover
+from proposition import PropParseTree, parser as prop_parser
+
+
+def main():
+    prop_parse_tree = PropParseTree()
+    while True:
+        try:
+            goal = prop_parse_tree.transform(prop_parser.parse(input('Proposition? < ')))
+        except Exception as e:
+            print(e)
+        else:
+            break
+    prover = Prover(goal)
+    tactic_dict = {
+        "undo": (prover.undo, 0),
+        "intro": (prover.intro, 0),
+        "assumption": (prover.assumption, 0),
+        "apply": (prover.apply, 1),
+        "left": (prover.left, 0),
+        "right": (prover.right, 0),
+        "destruct": (prover.destruct, 1),
+        "specialize": (prover.specialize, 2),
+        "add_dn": (prover.add_dn, 0)
+    }
+    while True:
+        if prover.goal is None:
+            print('No more goals.')
+            print()
+            break
+        else:
+            print(f'{len(prover.subgoals)+1} goal{"s" if prover.subgoals else ""}')
+            print()
+            for i, v_type in enumerate(prover.variables):
+                print(f'  H{i} : {str(v_type)}')
+            print('  ============================')
+            print(f'  {prover.goal}')
+            print()
+            for i, goal in enumerate(prover.subgoals):
+                print(f'goal {i+2} is:')
+                print(goal[0])
+                print()
+        tactics = input('pyprover < ')
+        if not tactics:
+            continue
+        for tactic in tactics.split(";"):
+            words = tactic.split()
+            if not words:
+                continue
+            elif words == ['auto']:
+                if results := prover.auto():
+                    for result in results:
+                        print(f'auto: {result}')
+                else:
+                    print(f'auto: tactic failed(Could not find proof)')
+            elif words[0] == 'auto_classical':
+                if len(words) == 1:
+                    if results := prover.auto_classical():
+                        for result in results:
+                            print(f'auto_classical: {result}')
+                    else:
+                        print(f'auto_classical: tactic failed(Could not find proof)')
+                elif len(words) != 2:
+                    print(f'{tactic}: 0 or 1 argument expected but given {len(words) - 1}')
+                    continue
+                elif not words[1].isdigit():
+                    print(f'{tactic}: invalid argument')
+                elif results := prover.auto_classical(int(words[1])):
+                    for result in results:
+                        print(f'auto_classical: {result}')
+                else:
+                    print(f'auto: tactic failed(Could not find proof)')
+            elif words[0] not in tactic_dict:
+                print(f'{tactic}: invalid tactic')
+                continue
+            elif len(words)-1 != tactic_dict[words[0]][1]:
+                print(f'{tactic}: {tactic_dict[words[0]][1]} argument(s) expected but given {len(words)-1}')
+                continue
+            elif not all(map(lambda arg: arg.isdigit(), words[1:])):
+                print(f'{tactic}: invalid argument(s)')
+                continue
+            elif tactic_dict[words[0]][0](*map(int, words[1:])):
+                print(f'{tactic}: tactic failed')
+
+
+if __name__ == '__main__':
+    main()
---
+++ pyprover/prover.py
@@ -0,0 +1,208 @@
+from copy import deepcopy
+from proposition import PropNode, then_symbol, and_symbol, or_symbol
+
+
+def dn(prop: PropNode):
+    return PropNode("→", PropNode("→", prop, PropNode("False")), PropNode("False"))
+
+
+class Prover:
+    def __init__(self, goal: PropNode):
+        self.goal = goal
+        self.variables = []
+        self.subgoals = []  # [(goal, variables), ...]
+        self.undos = []
+
+    def __set_undo(self):
+        self.undos.append(deepcopy((self.goal, self.variables, self.subgoals)))
+
+    def undo(self) -> bool:
+        if not self.undos:
+            return True
+        else:
+            (self.goal, self.variables, self.subgoals) = self.undos.pop()
+            return False
+
+    def assumption(self) -> bool:
+        if self.goal in self.variables:
+            self.__set_undo()
+            self.goal = None
+            if self.subgoals:
+                (self.goal, self.variables) = self.subgoals.pop()
+            return False
+        else:
+            return True
+
+    def intro(self) -> bool:
+        if self.goal.symbol == then_symbol:
+            self.__set_undo()
+            self.variables.append(self.goal.left)
+            self.goal = self.goal.right
+            return False
+        else:
+            return True
+
+    def apply(self, number: int) -> bool:
+        if len(self.variables) <= number:
+            return True
+        elif (self.variables[number].symbol == then_symbol
+              and self.variables[number].right == self.goal):
+            self.__set_undo()
+            self.goal = self.variables[number].left
+            return False
+        else:
+            return True
+
+    def split(self) -> bool:
+        if self.goal.symbol == and_symbol:
+            self.__set_undo()
+            self.subgoals.append((self.goal.right, self.variables))
+            self.goal = self.goal.left
+            return False
+        else:
+            return True
+
+    def left(self):
+        if self.goal.symbol == or_symbol:
+            self.__set_undo()
+            self.goal = self.goal.left
+            return False
+        else:
+            return True
+
+    def right(self):
+        if self.goal.symbol == or_symbol:
+            self.__set_undo()
+            self.goal = self.goal.right
+            return False
+        else:
+            return True
+
+    def destruct(self, number: int) -> bool:
+        if len(self.variables) <= number:
+            return True
+        elif self.variables[number].symbol == and_symbol:
+            self.__set_undo()
+            self.variables.append(self.variables[number].left)
+            self.variables.append(self.variables[number].right)
+            del self.variables[number]
+            return False
+        elif self.variables[number].symbol == or_symbol:
+            self.__set_undo()
+            temp = self.variables[:]
+            temp[number] = temp[number].right
+            self.subgoals.append((self.goal, temp))
+            self.variables[number] = self.variables[number].left
+            return False
+        elif self.variables[number].symbol == 'False':
+            self.__set_undo()
+            self.goal = None
+            if self.subgoals:
+                (self.goal, self.variables) = self.subgoals.pop()
+            return False
+        else:
+            return True
+
+    def specialize(self, function, domain) -> bool:
+        if len(self.variables) <= min(function, domain):
+            return True
+        elif (self.variables[function].symbol != "→"
+              or self.variables[function].left != self.variables[domain]):
+            return True
+        else:
+            self.__set_undo()
+            self.variables[function] = self.variables[function].right
+            return False
+
+    def add_dn(self):
+        self.__set_undo()
+        self.goal = dn(self.goal)
+        return False
+
+    def auto(self) -> [str]:
+        if not self.assumption():
+            if self.goal is None:
+                return ["assumption"]
+            if ans := self.auto():
+                return ["assumption"] + ans
+            self.undo()
+        if not self.intro():
+            if ans := self.auto():
+                return ["intro"] + ans
+            self.undo()
+        if not self.split():
+            if ans := self.auto():
+                return ["split"] + ans
+            self.undo()
+        if not self.left():
+            if ans := self.auto():
+                return ["left"] + ans
+            self.undo()
+        if not self.right():
+            if ans := self.auto():
+                return ["right"] + ans
+            self.undo()
+        for i in range(len(self.variables)):
+            if not self.destruct(i):
+                if self.goal is None:
+                    return [f"destruct {i}"]
+                if ans := self.auto():
+                    return [f"destruct {i}"] + ans
+                self.undo()
+            if not self.apply(i):
+                if ans := self.auto():
+                    return [f"apply {i}"] + ans
+                self.undo()
+            for j in range(len(self.variables)):
+                if not self.specialize(i, j):
+                    if ans := self.auto():
+                        return [f"specialize {i} {j}"] + ans
+                    self.undo()
+        return []
+
+    def auto_classical(self, depth_limit: int = 10) -> [str]:
+        if depth_limit <= 0:
+            return []
+        if not self.assumption():
+            if self.goal is None:
+                return ["assumption"]
+            if ans := self.auto_classical(depth_limit-1):
+                return ["assumption"] + ans
+            self.undo()
+        if not self.intro():
+            if ans := self.auto_classical(depth_limit-1):
+                return ["intro"] + ans
+            self.undo()
+        if not self.split():
+            if ans := self.auto_classical(depth_limit-1):
+                return ["split"] + ans
+            self.undo()
+        if not self.left():
+            if ans := self.auto_classical(depth_limit-1):
+                return ["left"] + ans
+            self.undo()
+        if not self.right():
+            if ans := self.auto_classical(depth_limit-1):
+                return ["right"] + ans
+            self.undo()
+        for i in range(len(self.variables)):
+            if not self.destruct(i):
+                if self.goal is None:
+                    return [f"destruct {i}"]
+                if ans := self.auto_classical(depth_limit-1):
+                    return [f"destruct {i}"] + ans
+                self.undo()
+            if not self.apply(i):
+                if ans := self.auto_classical(depth_limit-1):
+                    return [f"apply {i}"] + ans
+                self.undo()
+            for j in range(len(self.variables)):
+                if not self.specialize(i, j):
+                    if ans := self.auto_classical(depth_limit-1):
+                        return [f"specialize {i} {j}"] + ans
+                    self.undo()
+        if not self.add_dn():
+            if ans := self.auto_classical(depth_limit-1):
+                return ["add_dn"] + ans
+            self.undo()
+        return []
